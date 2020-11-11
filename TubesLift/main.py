dbca53f91d8d51369b5ed02bb0d6023720dd8178
Riandy Hasan <16520117@mahasiswa.itb.ac.id>
2020-11-11 22:58:58 +0800

    edit 1

--- TubesLift/main.py
+++ TubesLift/main.py
@@ -1,20 +1,43 @@
 import eel
+import time
 
 lantai = 1
+lift = 1
 
 @eel.expose
 def naik():
     global lantai
     lantai += 1
+    if (lantai >= 6):
+        lantai = 6
     eel.update_text(str(lantai))
-    if (lantai == 4):
-        eel.init('lift')
-        eel.start('dalam.html', size=(320,520))
-    
+
+@eel.expose
 def turun():
     global lantai
     lantai -= 1
+    if (lantai <= 0):
+        lantai = 1
     eel.update_text(str(lantai))
+
+@eel.expose
+def liftnaik ():
+    global lantai
+    global lift
+    while (lift < lantai):
+        lift += 1
+        time.sleep(1)
+        eel.update_lift(str(lift))
+
+@eel.expose
+def liftturun ():
+    global lantai
+    global lift
+    while (lift > lantai):
+        lift -= 1
+        time.sleep(1)
+        eel.update_lift(str(lift))
+
 
 
 eel.init('lift')
